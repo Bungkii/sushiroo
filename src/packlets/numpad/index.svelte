a3d3944f4178366f883003c431f93af10e0b3995
Bungkii <30260@student.act.ac.th>
2026-04-08 08:06:23 +0700

    Update footer copyright and links in index.svelte

--- src/packlets/numpad/index.svelte
+++ src/packlets/numpad/index.svelte
@@ -15,6 +15,6 @@
     {/each}
   </ul>
   <footer>
-    <p>&copy; 2024 - {new Date().getFullYear()} Phumrapee L. | <a href="https://rayriffy.com" target="_blank" rel="noopener noreferrer">Home</a> <a href="https://github.com/rayriffy/sushiro" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+    <p>&copy; 2026 - {new Date().getFullYear()} Bungkiii | <a href="https://github.com/Bungkii" target="_blank" rel="noopener noreferrer">GitHub</a></p>
   </footer>
 </section>
